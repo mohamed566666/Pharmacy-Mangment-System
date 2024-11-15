780da22f97185b950d4ddf199763f7c3e9b64fdf
Mohamed Gamal <115277088+mohamed566666@users.noreply.github.com>
2024-11-15 23:32:01 +0200

    Update Inventory.java

--- Project/src/Inventory.java
+++ Project/src/Inventory.java
@@ -1,27 +1,32 @@
+import java.util.*;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
 
 public class Inventory {
     private HashMap<String, Product> products;
 
+    // constructor
     public Inventory() {
         products = new HashMap<>();
     }
 
+    // method to add a specific product to (products)hashmap
     public void addProduct(Product product) {
         products.put(product.getProductId(), product);
     }
 
+    // method to remove a specific product from (products)hashmap
     public void removeproduct(Product product) {
-        if (products.get(product.getProductId())!=null)
+        if (products.get(product.getProductId()) != null)
             products.remove(product.getProductId());
     }
 
+    // method to return all products Available in inventory
     public HashMap<String, Product> getAllProducts() {
         return products;
     }
 
+    // method to update Quantity of a specific product
     public void updateQuantity(Product product, int quantity, boolean add) {
         if (add) {
             product.increasequantity(quantity);
@@ -32,10 +37,12 @@ public class Inventory {
         }
     }
 
+    // method to return a specific product by its productID
     public Product getProduct(String productId) {
         return products.get(productId);
     }
 
+    // method to return boolean flag to answer query if product is available or not
     public boolean isProductAvailable(String productId, int quantity) {
         Product product = products.get(productId);
         return (product != null) && (product.getQuantity() >= quantity);
@@ -44,13 +51,12 @@ public class Inventory {
     public void writeInventoryToFile(String fileName) {
         FileWriter writer = null;
         try {
-            writer = new FileWriter(fileName, true); // 'true' to append to the file
-            for (Product product : products.values()) {
+            writer = new FileWriter(fileName, true);
+            for (var product : products.values()) {
                 writer.write("Product ID: " + product.getProductId() +
                         ", Name: " + product.getName() +
                         ", Quantity: " + product.getQuantity() + "\n");
             }
-
             System.out.println("Inventory has been written to file: " + fileName);
         } catch (IOException e) {
             System.out.println("An error occurred while writing the inventory to the file.");
@@ -58,7 +64,7 @@ public class Inventory {
         } finally {
             try {
                 if (writer != null) {
-                    writer.close(); // Close the FileWriter to release resources
+                    writer.close();
                 }
             } catch (IOException e) {
                 System.out.println("An error occurred while closing the file.");
@@ -67,11 +73,14 @@ public class Inventory {
         }
     }
 
+    // method to desplay all products available in this Inventory
     public void display() {
-
-        for (Product product : products.values()) {
-            product.display();
+        if (products.isEmpty()) {
+            System.out.println("No products in inventory.");
+        } else {
+            for (var product : products.values()) {
+                product.display();
+            }
         }
     }
-
 }
