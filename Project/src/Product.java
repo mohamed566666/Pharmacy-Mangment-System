63d532332ca52a4b181ab0874ac597fbe5cacc45
Mohamed Gamal <115277088+mohamed566666@users.noreply.github.com>
2024-11-15 23:30:13 +0200

    Update Product.java

--- Project/src/Product.java
+++ Project/src/Product.java
@@ -4,6 +4,7 @@ public class Product {
   private double price;
   private int quantity;
 
+  // constructor
   public Product(String name, String productId, double price, int quantity) {
     this.name = name;
     this.productId = productId;
@@ -11,37 +12,40 @@ public class Product {
     this.quantity = quantity;
   }
 
+  // method to get the name of product
   public String getName() {
     return name;
   }
 
+  // method to get the ID of product
   public String getProductId() {
     return productId;
   }
 
+  // method to get the Price of product
   public double getPrice() {
     return price;
   }
 
+  // method to get the Available Quantity of product
   public int getQuantity() {
     return quantity;
   }
 
-  public void increasequantity(int amount) // exception
-  {
-    if (amount > 0)
+  // method to increase Quantity of The product
+  public void increasequantity(int amount) {
+    if (amount > 0) // check that amount is positive
       quantity += amount;
   }
 
-  public void decreasequantity(int amount) // exception
-  {
-    if (quantity >= amount)
+  // method to decrease Quantity of The product
+  public void decreasequantity(int amount) {
+    if (quantity >= amount) // check that amount is smaller than Available Quantity to Avoid get Negative Quantity
       quantity -= amount;
   }
 
+  // method to display the details of this product
   public void display() {
-    System.out.println("Product ID: " + getProductId() +
-        ", Name: " + getName() +
-        ", Quantity: " + getQuantity());
+    System.out.println("Product ID: " + getProductId() + ", Name: " + getName() + ", Quantity: " + getQuantity());
   }
 }
