f931d3af8937ca6df0c0cb905e12c7380a728ac8
Mohamed Gamal <115277088+mohamed566666@users.noreply.github.com>
2024-11-15 23:32:40 +0200

    Update ManageCustomerDiscount.java

--- Project/src/ManageCustomerDiscount.java
+++ Project/src/ManageCustomerDiscount.java
@@ -3,14 +3,17 @@ import java.util.HashMap;
 public class ManageCustomerDiscount {
     private HashMap<String, Integer> frequencyForCustomer;
 
+    // constructor
     public ManageCustomerDiscount() {
         this.frequencyForCustomer = new HashMap<>();
     }
 
+    // method to add customer
     public void addCustomer(String id) {
-        frequencyForCustomer.put(id, frequencyForCustomer.get(id) + 1);
+        frequencyForCustomer.put(id, 1);
     }
 
+    // method to check if this customer
     public void checkAndUpdate(String id) {
         if (!frequencyForCustomer.containsKey(id)) {
             addCustomer(id);
@@ -19,14 +22,13 @@ public class ManageCustomerDiscount {
         }
     }
 
+    // method to calculate the percentage of discount by using Number of visits
     public double calculateDiscount(String id) {
         if (!frequencyForCustomer.containsKey(id)) {
             return 0;
         }
-
         int purchaseFrequency = frequencyForCustomer.get(id);
         double discount;
-
         if (purchaseFrequency >= 20) {
             discount = 0.20;
         } else if (purchaseFrequency >= 10) {
@@ -36,7 +38,6 @@ public class ManageCustomerDiscount {
         } else {
             discount = 0.0;
         }
-
         return discount;
     }
 }
