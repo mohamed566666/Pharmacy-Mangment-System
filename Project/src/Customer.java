e44cbeb3130e01d0adc3450b2e0954834e58b980
Mohamed Gamal <115277088+mohamed566666@users.noreply.github.com>
2024-11-15 23:31:02 +0200

    Update Customer.java

--- Project/src/Customer.java
+++ Project/src/Customer.java
@@ -1,29 +1,33 @@
 public class Customer {
     private String name;
-    private String  customerId;
+    private String customerId;
     private String phone;
 
-    public Customer(String name, String customerId, double discount, String phone) {
+    // constructor
+    public Customer(String name, String customerId, String phone) {
         this.name = name;
         this.customerId = customerId;
         this.phone = phone;
     }
 
+    // method to get the name of this customer
     public String getname() {
         return name;
     }
 
+    // method to get ID of this customer
     public String getcustomerId() {
         return customerId;
     }
 
+    // method to get Phone Number of this customer
     public String getphone() {
         return phone;
     }
 
+    // method to display All details of this customer
     public void display() {
-        System.out.println(
-                "name : " + getname() + "\n" + "phone : " + getphone() + "\n" + "customerId : " + getcustomerId());
+        System.out.println("name : " + getname() + "\n" + "phone : " + getphone() + "\n" + "customerId : " + getcustomerId());
     }
 
 }
