74fc76ade245c03b3a0546e0de2e3f70bcec41c6
Mohamed Gamal <115277088+mohamed566666@users.noreply.github.com>
2024-11-15 23:33:07 +0200

    Update Login.java

--- Project/src/Login.java
+++ Project/src/Login.java
@@ -4,22 +4,28 @@ public class Login {
     private String username;
     private String password;
     private boolean isLogin;
-    private static HashMap<String, String> acceptedDoctors;
-
+    private static HashMap<String, String> acceptedDoctors; // must be Static
+    // Static block to initialize accepted doctors
     static {
         acceptedDoctors = new HashMap<>();
-        acceptedDoctors.put("Admin1", "123456789");
+        acceptedDoctors.put("Mohamed_Gamal", "mogamal17");
+        acceptedDoctors.put("Kareem_badr", "KBR2810");
+        acceptedDoctors.put("Abdo_Hamada", "123456789");
+        acceptedDoctors.put("Zizo", "meshZezo74");
     }
 
+    // constructor
     public Login(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
+    // method to validate if this user and passowrd correct
     private boolean validation() {
-        return acceptedDoctors.containsKey(username) && acceptedDoctors.get(username).equals(password);
+        return (acceptedDoctors.containsKey(username) && acceptedDoctors.get(username).equals(password));
     }
 
+    // method to check process of Login
     public boolean login() {
         if (validation()) {
             isLogin = true;
@@ -31,20 +37,18 @@ public class Login {
         }
     }
 
+    // method to logout from System
     public void logout() {
         if (isLogin) {
             isLogin = false;
             System.out.println("Logged out successfully.");
-        } else {
+        } else { // for exception
             System.out.println("You are not logged in.");
         }
     }
 
+    // boolean method to return the Statuas of Login
     public boolean loginStatus() {
         return isLogin;
-    }
-
-    public static void acceptedDoctorFile() {
-
     }
 }
